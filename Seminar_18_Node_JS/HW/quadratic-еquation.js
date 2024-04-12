ae2e29c21f66607e3895e48840531ec27ee17f5f
EvgenyVolenko <evolenko@inbox.ru>
2024-04-12 23:00:11 +0300

    Added Seminar_18_Node_JS & HW

--- Seminar_18_Node_JS/HW/quadratic-еquation.js
+++ Seminar_18_Node_JS/HW/quadratic-еquation.js
@@ -7,8 +7,7 @@ const calcResults = function (a, b, c) {
     if (d < 0) {
         return null;
     } else {
-        const results = [(- b + Math.sqrt(d)) / 2 / a, (- b - Math.sqrt(d)) / 2 / a];
-        return results;
+        return [(- b + Math.sqrt(d)) / 2 / a, (- b - Math.sqrt(d)) / 2 / a];
     }
 }
 

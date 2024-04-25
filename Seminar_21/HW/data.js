dd8016c42bf66863fd1e4b1a6cf648e8bc2bb390
EvgenyVolenko <evolenko@inbox.ru>
2024-04-25 14:13:25 +0300

    Started HW

---
+++ Seminar_21/HW/data.js
@@ -0,0 +1,26 @@
+const data = [
+    {
+        id: 1,
+        nameShedule: 'Йога',
+        timeStart: '10-00',
+        timeEnd: '12-00',
+        placesTotal: 30,
+        placesOccupied: 15
+    },
+    {
+        id: 2,
+        nameShedule: 'Каратэ',
+        timeStart: '13-00',
+        timeEnd: '15-00',
+        placesTotal: 20,
+        placesOccupied: 20
+    },
+    {
+        id: 3,
+        nameShedule: 'Пилатес',
+        timeStart: '21-00',
+        timeEnd: '23-00',
+        placesTotal: 30,
+        placesOccupied: 0
+    }
+];

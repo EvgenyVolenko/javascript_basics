cb94a067a64b3f615fdfce79eeba15b05d7e71a1
EvgenyVolenko <evolenko@inbox.ru>
2024-05-29 23:23:47 +0300

    added seminar_26 HW 0.8

--- Seminar_26/HW/script.js
+++ Seminar_26/HW/script.js
@@ -20,27 +20,101 @@ const addButton = document.querySelector('.add-button');
 const productList = document.querySelector('.product-list');
 const feedackList = document.querySelector('.feedack-list');
 
-const fillFeedbacks = function (block, productFeedbacks) {
+const deleteFeedbackFunc = function (product, feedbackItem) {
+
+    for (let i = 0; i < product.feedbacks.length; i++) {
+        if (product.feedbacks[i].id === feedbackItem.id) {
+            product.feedbacks.splice(i, 1);
+        }
+    }
+}
+
+const countId = function (productsD) {
+    let counter = 0;
+    productsD.forEach(el => {
+        el.feedbacks.forEach(feedback => {
+            if (feedback.id > counter) {
+                counter = feedback.id;
+            }
+        });
+    });
+    return counter;
+}
+
+const fillFeedbacks = function (block, product) {
     block.innerHTML = '';
-    productFeedbacks.forEach(feedbackItem => {
+    product.feedbacks.forEach(feedbackItem => {
         const feedback = document.createElement('li');
         const deleteFeedbackBtn = document.createElement('button');
         deleteFeedbackBtn.textContent = 'Удалить отзыв';
         deleteFeedbackBtn.classList.add('delete-button');
+        deleteFeedbackBtn.addEventListener('click', function () {
+            deleteFeedbackFunc(product, feedbackItem);
+            fillFeedbacks(block, product);
+            localStorage.setItem('myData', JSON.stringify(productsData));
+        });
         feedback.textContent = feedbackItem.text;
         block.append(feedback);
         block.append(deleteFeedbackBtn);
     });
 }
 
-startProductsData.forEach(product => {
-    const productTitle = document.createElement('li');
-    productTitle.textContent = product.title;
-    productTitle.style.cursor = 'pointer';
-    productTitle.addEventListener('click', () => {
-        fillFeedbacks(feedackList, product.feedbacks);
+const fillProducts = function (block, products, feedackList) {
+    block.innerHTML = '';
+    products.forEach(product => {
+        const productTitle = document.createElement('li');
+        productTitle.textContent = product.title;
+        productTitle.style.cursor = 'pointer';
+        productTitle.addEventListener('click', () => {
+            fillFeedbacks(feedackList, product);
+        });
+        block.append(productTitle);
     });
-    productList.append(productTitle);
+}
+
+const checkProduct = function (data, productTitle) {
+    let findedProd = null;
+    data.forEach(el => {
+        if (el.title.toUpperCase() === productTitle.toUpperCase()) {
+            findedProd = el;
+        }
+    });
+    return findedProd;
+}
+
+let productsData = [];
+
+if (!localStorage.getItem('myData')) {
+    localStorage.setItem('myData', JSON.stringify(startProductsData));
+    productsData = startProductsData;
+} else {
+    productsData = JSON.parse(localStorage.getItem('myData'));
+}
+
+fillProducts(productList, productsData, feedackList);
+
+addButton.addEventListener('click', function () {
+    const productTitle = inputTitle.value;
+    const productFeedback = inputFeedback.value;
+
+    if (!productTitle || !productFeedback) {
+        alert('Вы ввели не все данные');
+    } else {
+        const newId = countId(productsData) + 1;
+        const findedProduct = checkProduct(productsData, productTitle);
+
+        if (findedProduct) {
+            findedProduct.feedbacks.push({ id: newId, text: productFeedback });
+            localStorage.setItem
+            fillFeedbacks(feedackList, findedProduct);
+        } else {
+            const newFeedbacks = [{ id: newId, text: productFeedback }];
+            productsData.push({ title: productTitle, feedbacks: newFeedbacks });
+            fillProducts(productList, productsData, feedackList);
+            fillFeedbacks(feedackList, { title: productTitle, feedbacks: newFeedbacks });
+        }
+        localStorage.setItem('myData', JSON.stringify(productsData));
+    }
+    inputTitle.value = '';
+    inputFeedback.value = ''
 });
-
-
